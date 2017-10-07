b7905eb16890c4572f35a245056df5b5d9a87757
Ulysses <Ulysses@SOIER>
2017-10-07 14:27:06 +0200

    Добавьте файлы проекта.

---
+++ KateProg2/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace KateProg2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Ubuntu", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(270, 105);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Main Words";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Ubuntu", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 134);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(270, 340);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "Second Words";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("Ubuntu", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox3.Location = new System.Drawing.Point(288, 23);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(326, 451);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Ubuntu Light", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(620, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 62);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Ubuntu Light", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(620, 414);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 60);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Find";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KateProg2.Properties.Resources.Picture;
+            this.pictureBox1.Location = new System.Drawing.Point(620, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 317);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ubuntu", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "No File Selected";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ubuntu", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 477);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "No File Selected";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(738, 502);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 500);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "KateWords2";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_onDragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_onDragEnter);
+            this.DragLeave += new System.EventHandler(this.Form1_onDragLeave);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ KateProg2/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+using Independentsoft.Office.Odf;
+
+namespace KateProg2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadFonts();
+        }
+
+        PrivateFontCollection font;
+        private void loadFonts()
+        {
+            this.font = new PrivateFontCollection();
+            this.font.AddFontFile("font/Ubuntu.ttf");
+            this.font.AddFontFile("font/Ubuntu Light.ttf");
+        }
+
+        Document document = new Document();
+
+        private void readText(string fileName)
+        {
+            label1.Text = fileName;
+
+            //Read file
+            //if .rtf
+            if (fileName.Substring(fileName.Length - 5, 5).Contains("rtf"))
+            {
+                document.textBox.Rtf = File.ReadAllText(fileName);
+            }
+            //if .pdf
+            else if (fileName.Substring(fileName.Length - 5, 5).Contains("pdf"))
+            {
+                PDDocument doc = null;
+                try
+                {
+                    doc = PDDocument.load(fileName);
+                    PDFTextStripper stripper = new PDFTextStripper();
+                    document.textBox.Text = stripper.getText(doc);
+                }
+                finally
+                {
+                    if (document != null)
+                    {
+                        doc.close();
+                    }
+                }
+            }
+            //if .odt
+            else if (fileName.Substring(fileName.Length - 5, 5).Contains("odt"))
+            {
+                TextDocument doc = new TextDocument(fileName);
+                document.textBox.Text = doc.ToText();
+            }
+            //if all else
+            else
+            {
+                using (StreamReader str = new StreamReader(fileName, Encoding.Default))
+                {
+                    while (!str.EndOfStream)
+                    {
+                        document.textBox.AppendText(str.ReadLine().ToLower() + "\r\n");
+                    }
+                }
+            }
+            //check counts
+            label2.Text = "Words in File ~ " + document.getWordsCount() + ",  Average Words in the Sentence: " + document.getAverageWordsInSentence();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //open the file
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            openFile.Filter = "Text (*.txt; *.doc; *.docx; *.odt; *.rtf; *.pdf; *.chm)|*.txt; *.doc; *.docx; *.odt; *.rtf; *.pdf; *.chm ";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                readText(openFile.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WordsPair words = new WordsPair();
+
+            words.setWords(richTextBox1.Text.Split('\r', '\n'), richTextBox2.Text.Split('\r', '\n'));
+
+            foreach (string line in document.textBox.Text.Split('\r', '\n'))
+            {
+                if (line.Split(' ').Length != 0 && line.Split(' ')[0] != "")
+                {
+                    words.checkWords(line.Split(' '), document.getAverageWordsInSentence());
+                }
+            }
+
+            richTextBox3.Text = words.getResults();
+        }
+
+        private void Form1_onDragEnter(object sender, DragEventArgs e)
+        {
+            pictureBox2.Visible = true;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Form1_onDragDrop(object sender, DragEventArgs e)
+        {
+            pictureBox2.Visible = false;
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            readText(files[0]);
+        }
+
+        private void Form1_onDragLeave(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+        }
+    }
+
+    public class Document
+    {
+        public RichTextBox textBox = new RichTextBox();
+        int averageWordsInSentence;
+        int wordsCount;
+
+        public Document()
+        {
+            averageWordsInSentence = 0;
+            wordsCount = 0;
+        }
+
+        public int getAverageWordsInSentence()
+        {
+            if(averageWordsInSentence == 0)
+            {
+                averageWordsInSentence = textBox.Text.Split(' ').Length / textBox.Text.Split('.').Length;
+            }
+            return averageWordsInSentence;
+        }
+
+        public int getWordsCount()
+        {
+            if(wordsCount == 0)
+            {
+                wordsCount = textBox.Text.Split(' ').Length;
+            }
+            return wordsCount;
+        }
+
+    }
+
+    public class WordsPair
+    {
+        int[] foundCount;
+        bool[] foundFirst;
+        int[] currentBetween;
+        int[] firstWordsCount;
+        int[] secondWordsCount;
+        string[] currentWord;
+        Dictionary<string, int> words = new Dictionary<string, int>();
+
+        public WordsPair()
+        {
+
+        }
+
+        public void setWords(string[] firstWords, string[] secondWords)
+        {
+            int i = 0;
+            foundCount = new int[firstWords.Length * secondWords.Length];
+            foundFirst = new bool[firstWords.Length * secondWords.Length];
+            currentBetween = new int[firstWords.Length * secondWords.Length];
+            firstWordsCount = new int[firstWords.Length * secondWords.Length];
+            secondWordsCount = new int[firstWords.Length * secondWords.Length];
+            currentWord = new string[firstWords.Length * secondWords.Length];
+
+            foreach (string first in firstWords)
+            {
+                foreach (string second in secondWords)
+                {
+                    if (first != "" && second != "")
+                    {
+                        foundCount[i] = 0;
+                        foundFirst[i] = false;
+                        currentBetween[i] = 0;
+                        firstWordsCount[i] = first.Split(' ').Length;
+                        secondWordsCount[i] = second.Split(' ').Length;
+                        currentWord[i] = "";
+                        words.Add(first.ToLower() + "  " + second.ToLower(), i++);
+                    }
+                }
+            }
+        }
+
+        public void checkWords(string[] line, int averageWordsInSentence)
+        {
+            string firstWord;
+            string secondWord;
+            string nextWord;
+            int countWordsSearching;
+            int spacePos;
+            int secondWordLength;
+
+            foreach (KeyValuePair<string, int> word in words)
+            {
+                spacePos = word.Key.IndexOf("  ");
+                secondWordLength = word.Key.Length - spacePos - 2;
+                firstWord = word.Key.Substring(0, spacePos);
+                secondWord = word.Key.Substring(spacePos + 2, secondWordLength);
+                countWordsSearching = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] != "")
+                    {
+                        nextWord = line[i].ToLower();
+
+                        if (!foundFirst[word.Value])
+                        {
+                            if (firstWord.Contains(nextWord) && countWordsSearching < firstWordsCount[word.Value])
+                            {
+                                countWordsSearching++;
+                                currentWord[word.Value] += nextWord + ' ';
+                            }
+                            else if (currentWord[word.Value].Contains(firstWord) && countWordsSearching >= firstWordsCount[word.Value])
+                            {
+                                foundFirst[word.Value] = true;
+                                countWordsSearching = 0;
+                                currentBetween[word.Value] = 0;
+                                currentWord[word.Value] = "";
+                            }
+                        }
+                        else
+                        {
+                            if (secondWord.Contains(nextWord) && currentBetween[word.Value] <= averageWordsInSentence && countWordsSearching < secondWordsCount[word.Value])
+                            {
+                                countWordsSearching++;
+                                currentWord[word.Value] += nextWord + ' ';
+                            }
+                            else if (currentWord[word.Value].Contains(secondWord))
+                            {
+                                currentWord[word.Value] = "";
+                                foundFirst[word.Value] = false;
+                                currentBetween[word.Value] = 0;
+                                foundCount[word.Value]++;
+                            }
+                            else if (currentBetween[word.Value] != averageWordsInSentence)
+                            {
+                                currentBetween[word.Value]++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public string getResults()
+        {
+            RichTextBox results = new RichTextBox();
+            foreach (KeyValuePair<string, int> word in words)
+            {
+                results.AppendText(word.Key + ' ' + '(' + foundCount[word.Value] +')' + '\r' + '\n');
+            }
+            return results.Text;
+        }
+
+    }
+
+}
